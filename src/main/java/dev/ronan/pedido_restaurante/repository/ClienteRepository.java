2a51abea5f106d11aa2fe6956be573fc73727202
RonanAlecrim <ronancabral02@gmail.com>
2026-05-29 02:26:05 -0300

    feat: criação da camada service e impletação do método cadastrarCliente

--- src/main/java/dev/ronan/pedido_restaurante/repository/ClienteRepository.java
+++ src/main/java/dev/ronan/pedido_restaurante/repository/ClienteRepository.java
@@ -3,7 +3,11 @@ package dev.ronan.pedido_restaurante.repository;
 import dev.ronan.pedido_restaurante.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
+    Optional<Cliente> findById(UUID id);
+    Optional<Cliente> findByNome(String nome);
+    Optional<Cliente> findByEmail(String email);
 }
---
+++ src/main/java/dev/ronan/pedido_restaurante/service/ClienteService.java
@@ -0,0 +1,37 @@
+package dev.ronan.pedido_restaurante.service;
+
+import dev.ronan.pedido_restaurante.dto.request.ClienteRequestDTO;
+import dev.ronan.pedido_restaurante.dto.response.ClienteResponseDTO;
+import dev.ronan.pedido_restaurante.model.Cliente;
+import dev.ronan.pedido_restaurante.repository.ClienteRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    ClienteResponseDTO cadastrarCliente(ClienteRequestDTO clienteDTO){
+
+        if (clienteRepository.findByEmail(clienteDTO.email()).isPresent()) {
+            throw new RuntimeException("Este e-mail já está cadastrado!");
+        }
+
+        Cliente clienteSalvo = new Cliente();
+        clienteSalvo.setNome(clienteDTO.nome());
+        clienteSalvo.setEmail(clienteDTO.email());
+        clienteSalvo.setSenha(clienteDTO.senha());
+        clienteRepository.save(clienteSalvo);
+
+        return new ClienteResponseDTO(
+                clienteSalvo.getIdCliente(),
+                clienteSalvo.getNome(),
+                clienteSalvo.getEmail()
+        );
+    }
+}
